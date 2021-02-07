76955404df225ab514b400364157565ef64d06c2
Houssem ABIDI <h.abidi@sofrecom-tunisie.com>
2021-02-07 17:52:42 +0100

    first commit

---
+++ back/src/test/java/com/sofrecom/pfe/collaborateur/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofrecom.pfe.collaborateur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
